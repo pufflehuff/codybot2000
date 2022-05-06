5efcdd057c32079e521b764bd58ea13eacf100ee
Zynh Ludwig <Zynh0722@gmail.com>
2022-05-05 17:26:58 -0700

    Init repo

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+      'jest/globals': true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'plugin:import/recommended',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      'better-styled-components',
+      'jest',
+    ],
+    rules: {
+      'no-console': 0,
+      'react/no-array-index-key': 0,
+      'react/forbid-prop-types': [2, { forbid: ['any'] }],
+      'react/jsx-boolean-value': 0,
+    },
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [
+            ['common', './client/src/components/common'],
+          ],
+          extensions: ['.js', '.jsx'],
+        },
+      },
+    },
+  };
+  
