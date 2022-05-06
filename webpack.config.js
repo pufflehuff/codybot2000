e9dc3f65cc907a548b4c716dfd74cd6a857ef464
Zynh Ludwig <Zynh0722@gmail.com>
2022-05-06 11:20:29 -0700

    architecture: cleanup regex

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         exclude: /nodeModules/,
         use: {
           loader: 'babel-loader',
