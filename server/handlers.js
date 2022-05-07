9cf853e5ad442515178de86692d023ed608ad4b5
Zynh Ludwig <Zynh0722@gmail.com>
2022-05-07 13:47:50 -0700

    fill out the rest of these starter routes

--- server/handlers.js
+++ server/handlers.js
@@ -1,3 +1,5 @@
 module.exports = {
   problems: () => 'you would normally put a db query here',
+  problem: () => 'you would normally put a db query here',
+  comments: () => 'you would normally put a db query here',
 };
--- server/routes.js
+++ server/routes.js
@@ -5,3 +5,15 @@ module.exports.problems = (res, req) => {
     .then((data) => res.json(data))
     .catch((err) => res.status(500).json(err));
 };
+
+module.exports.problem = (res, req) => {
+  handlers.problem(res, req)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err));
+};
+
+module.exports.comments = (res, req) => {
+  handlers.comments(res, req)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err));
+};
