5d1c1752e0990c73251d49f159da110f18c9df25
Zynh Ludwig <Zynh0722@gmail.com>
2022-05-07 12:29:12 -0700

    Finish merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
 import TempApp from './components/list/TempApp';
 // import App.css;
-=======
-import TempApp from './components/list/TempApp.jsx';
-import App.css;
->>>>>>> main
 
 createRoot(document.getElementById('root')).render(
   <TempApp />,
