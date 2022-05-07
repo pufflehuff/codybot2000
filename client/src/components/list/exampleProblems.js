15a4186f5e8274f21182f2bad582776380eb6c9b
Kamran Masood <kam1088@gmail.com>
2022-05-07 16:14:27 -0700

    down with linting

--- client/src/components/list/exampleProblems.js
+++ client/src/components/list/exampleProblems.js
@@ -52,10 +52,6 @@ const examples = [
     timestamp: '2022-5-6',
     author: 'leetcode',
   },
-
-
-
-
   {
     name: 'Trapping Rain Water',
     prompt: 'Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.',
