f98e6ed879e8519e6f595370134021ebab63fb09
Tristan L <tristan97little@gmail.com>
2022-05-09 11:34:30 -0600

    Added AddNewPrompt to App render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './Styles.css';
 import './components/list/ListStyles.css';
+import './components/addPrompt/AddPrompt.css';
 
 createRoot(document.getElementById('root')).render(
   <App />,
