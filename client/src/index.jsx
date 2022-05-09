8adc4dfab64cba78d3394e6a92e8ac42c7c18bb4
Andrew Tran <amtran150@gmail.com>
2022-05-09 11:56:07 -0700

    reverted react and react-dom to v17.0.2

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import reactDom from 'react-dom';
 import App from './components/App';
 import './Styles.css';
 import './components/list/ListStyles.css';
 
-createRoot(document.getElementById('root')).render(
+reactDom.render(
   <App />,
+  document.getElementById('root'),
 );
