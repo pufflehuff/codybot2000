6e42182f7fb1a11753eb1eac8793050f41ef4c19
Ian Hoffman <ianrhoffman1@gmail.com>
2022-05-10 12:44:12 -0700

    frontend[addPrompt]: adds state to track user inputs adds a few extra inputs to gather other needed user info

--- client/src/components/addPrompt/AddNewPrompt.jsx
+++ client/src/components/addPrompt/AddNewPrompt.jsx
@@ -1,26 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AddNewPrompt() {
+  const [promptName, setPromptName] = useState('');
+  const [promptBody, setPromptBody] = useState('');
+  const [testsBody, setTestsBody] = useState('');
+  const [dificulty, setDificulty] = useState('');
+  const [constraints, setConstraints] = useState('');
+
+  const handleSubmit = () => {
+    console.log(promptName, promptBody, testsBody);
+    // examples: body.examples,
+    // constraints: body.constraints,
+    // tags: body.tags,
+    // timestamp: body.timestamp,
+    //      author: body.author,
+
+    const newPrompt = {
+      name: promptName,
+      prompt: promptBody,
+      dificulty,
+      constraints,
+      rating: 0,
+      numRatings: 0,
+    };
+    console.log(newPrompt);
+  };
+
   return (
     <form className="PromptFrom">
 
-      <label htmlFor="promptName" className="PromptTopLabel">
-        <input type="text" name="propmptName" />
-        Prompt Name
+      Prompt Name
+      <label htmlFor="promptName" className="PromptLabel">
+        <input
+          type="text"
+          name="propmptName"
+          onChange={(event) => {
+            setPromptName(event.target.value);
+          }}
+        />
       </label>
 
-      <label htmlFor="promptBody" className="PromptTextLabel">
-        <textarea type="textarea" name="body" />
-        Prompt Body
+      Dificulty
+      <label htmlFor="promptDificulty" className="PromptLabel">
+        <select
+          onChange={(event) => {
+            setDificulty(event.target.value);
+          }}
+        >
+          <option>Easy</option>
+          <option>Medium</option>
+          <option>Hard</option>
+        </select>
       </label>
 
-      <label htmlFor="testsBody">
-        <textarea type="textarea" name="testsBody" className="PromptTextLabel" />
-        Tests Body
+      Prompt Body
+      <label htmlFor="promptBody" className="PromptLabel">
+        <textarea
+          type="textarea"
+          name="promptBody"
+          onChange={(event) => {
+            setPromptBody(event.target.value);
+          }}
+        />
+      </label>
+
+      Constraints
+      <label htmlFor="promptCOnstraints" className="PromptLabel">
+        <input
+          type="text"
+          name="propmptConstraints"
+          onChange={(event) => {
+            setConstraints(event.target.value);
+          }}
+        />
+      </label>
+
+      Tests Body
+      <label htmlFor="testsBody" className="PromptLabel">
+        <textarea
+          type="textarea"
+          name="testsBody"
+          onChange={(event) => {
+            setTestsBody(event.target.value);
+          }}
+        />
       </label>
 
       <div className="PromptButton">
-        <button type="submit">
+        <button
+          type="submit"
+          onClick={(event) => {
+            event.preventDefault();
+            handleSubmit();
+          }}
+        >
           Submit
         </button>
       </div>
