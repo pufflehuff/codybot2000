21e458dafa0902fc80fb9c94131aa08fd7e34f40
Robert Taylor <rvtaylor94@gmail.com>
2022-05-13 10:41:03 -0600

    backend[database]: replaced test message with actual data

--- server/routes.js
+++ server/routes.js
@@ -37,7 +37,7 @@ module.exports.userStats = (req, res) => {
     .then((data) => {
       if ((Date.now() - 86400000) > data.lastDateCompleted.getTime()) {
         handlers.getUserData(data, { streak: 0 })
-        .then((data) => res.json('deleted'))
+        .then((data) => res.json(data))
         .catch((err) => res.status(500).json(err));
       } else res.json(data);
     })
